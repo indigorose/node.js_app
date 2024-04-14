a6f5fa3c231d1d0a9c4c2b96510de14c829547f5
indigorose <indigorose049@gmail.com>
2024-04-14 15:24:46 +0100

    Add story schema and functionality tests

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
+
+// Story functionality
+
+const Story = require('../models/Story');
+
 // @desc    Login/Landing Page
 // @route   GET
 
@@ -11,10 +16,17 @@ router.get('/', ensureGuest, (req, res, next) => {
 // @desc    Dashboard
 // @route   GET /dashboard
 
-router.get('/dashboard', ensureAuth, (req, res, next) => {
-	res.render('../views/dashboard', {
-		name: req.user.firstName,
-	});
+router.get('/dashboard', ensureAuth, async (req, res, next) => {
+	try {
+		const stories = await Story.find({ user: req.user.id }).lean();
+		res.render('../views/dashboard', {
+			name: req.user.firstName,
+			stories,
+		});
+	} catch (err) {
+		console.error(err);
+		res.render('error/500');
+	}
 });
 
 module.exports = router;
